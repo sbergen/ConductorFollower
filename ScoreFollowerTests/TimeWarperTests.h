adaa20ab6bd71f8167b0c0d80931fb394551422f
Sakari Bergen <sakari.bergen@beatwaves.net>
2012-06-05 17:28:30 +0300

    Add Stub project for ScoreFollower tests

---
+++ ScoreFollowerTests/TimeWarperTests.h
@@ -0,0 +1,12 @@
+#include "TimeWarper.h"
+
+BOOST_AUTO_TEST_SUITE(TimeWarperTests)
+
+using namespace cf::ScoreFollower;
+
+BOOST_AUTO_TEST_CASE(Foo)
+{
+	
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ ScoreFollowerTests/main.cpp
@@ -0,0 +1,6 @@
+#define BOOST_TEST_MODULE ScoreFollower
+#include <boost/test/unit_test.hpp>
+
+// Test suites
+#include "TimeWarperTests.h"
+
