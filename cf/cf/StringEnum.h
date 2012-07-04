f953ea628e1df68ba25bf78da86af60d66769c9c
Sakari Bergen <sakari.bergen@beatwaves.net>
2012-07-04 16:02:00 +0300

    Add StringEnum and tests

---
+++ cf/cf/StringEnum.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <boost/preprocessor.hpp>
+
+#define CF_STRING_ENUM_visit(_r, _data, _i, _elem) \
+	BOOST_PP_CAT(CF_STRING_ENUM_, _data)(_i, BOOST_PP_TUPLE_ELEM(0, _elem), BOOST_PP_TUPLE_ELEM(1, _elem))
+
+#define CF_STRING_ENUM_enum_value(_i, _val, _str) BOOST_PP_COMMA_IF(_i) _val
+#define CF_STRING_ENUM_conversion_case(_i, _val, _str) case _val: return _str;
+
+#define CF_STRING_ENUM_enum_values(_seq) \
+	BOOST_PP_SEQ_FOR_EACH_I(CF_STRING_ENUM_visit, enum_value, _seq)
+
+#define CF_STRING_ENUM_conversion_cases(_seq) \
+	BOOST_PP_SEQ_FOR_EACH_I(CF_STRING_ENUM_visit, conversion_case, _seq)
+
+#define CF_STRING_ENUM_first_value(_seq) \
+	BOOST_PP_TUPLE_ELEM(0, BOOST_PP_SEQ_HEAD(_seq))
+
+#define CF_STRING_ENUM(_className, _seq) \
+	class _className \
+	{ \
+	public: \
+		enum Value { CF_STRING_ENUM_enum_values(_seq) }; \
+		_className () : value_( CF_STRING_ENUM_first_value(_seq) ) { } \
+		_className (Value val) : value_(val) { } \
+		_className & operator=(Value val) { value_ = val; return *this; } \
+		operator char const *() const { switch(value_) { CF_STRING_ENUM_conversion_cases(_seq)  } } \
+		operator Value() const { return value_; } \
+	private: \
+		Value value_; \
+	};
+	
---
+++ cfTests/StringEnumTests.cpp
@@ -0,0 +1,54 @@
+#include <boost/test/unit_test.hpp>
+
+#include "cf/StringEnum.h"
+
+BOOST_AUTO_TEST_SUITE(StringEnumTests)
+
+CF_STRING_ENUM(TestEnum,
+	((First, "First"))
+	((Second, "Second"))
+)
+
+BOOST_AUTO_TEST_CASE(TestConstruction)
+{
+	// default
+	TestEnum e1;
+	BOOST_CHECK_EQUAL(e1, TestEnum::First);
+
+	// from value
+	TestEnum e2(TestEnum::Second);
+	BOOST_CHECK_EQUAL(e2, TestEnum::Second);
+
+	// copy
+	TestEnum e3(e2);
+	BOOST_CHECK_EQUAL(e3, TestEnum::Second);
+}
+
+BOOST_AUTO_TEST_CASE(TestAssignment)
+{
+	TestEnum e1;
+	TestEnum e2(TestEnum::Second);
+
+	// Direct
+	BOOST_CHECK_NE(e1, TestEnum::Second);
+	e1 = e2;
+	BOOST_CHECK_EQUAL(e1, TestEnum::Second);
+
+	// from value
+	BOOST_CHECK_NE(e1, TestEnum::First);
+	e1 = TestEnum::First;
+	BOOST_CHECK_EQUAL(e1, TestEnum::First);
+}
+
+BOOST_AUTO_TEST_CASE(TestConversion)
+{
+	TestEnum e(TestEnum::Second);
+
+	// String
+	BOOST_CHECK_EQUAL(std::string(e), std::string("Second"));
+
+	// Value
+	BOOST_CHECK_EQUAL(e, TestEnum::Second);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
