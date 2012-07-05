16f085f3a2cc632e81f387dbd97202de9484dd43
Sakari Bergen <sakari.bergen@beatwaves.net>
2012-07-05 15:34:16 +0300

    Make lower_bound_inclusive even better :P

--- cf/cf/algorithm.h
+++ cf/cf/algorithm.h
@@ -1,18 +1,28 @@
 #include <algorithm>
+#include <functional>
+#include <iterator>
 
 namespace cf {
 
-// lower_bound is "first not less than", this is "last less than or equal"
-template<typename ForwardIterator, typename T>
-ForwardIterator lower_bound_inclusive(ForwardIterator first, ForwardIterator last, T const & value)
+// lower_bound_inclusive is "last less than or equal" (lower_bound is "first not less than")
+
+template<typename ForwardIterator, typename T, typename Compare>
+ForwardIterator lower_bound_inclusive(ForwardIterator first, ForwardIterator last, T const & value, Compare compare)
 {
-	ForwardIterator it = std::lower_bound(first, last, value);
+	ForwardIterator it = std::lower_bound(first, last, value, compare);
 	
 	if (it == last) { return --it; } // All items are smaller, return last valid one
 	// *it >= value at this stage
-	if (!(value < *it)) { return it; } // Equal (use only operator<)
+	if (!compare(value, *it)) { return it; } // Equal (use only operator<)
 	if (it == first) { return last; } // None less than or equal found
 	return --it; // This is the "default" case
+}
+
+template<typename ForwardIterator, typename T>
+ForwardIterator lower_bound_inclusive(ForwardIterator first, ForwardIterator last, T const & value)
+{
+	return lower_bound_inclusive(first, last, value,
+		std::less<typename std::iterator_traits<ForwardIterator>::value_type>());
 }
 
 } // namespace cf
