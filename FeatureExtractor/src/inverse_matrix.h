0cd424c2506c8091a1888dc99daefc1a364aed4f
Sakari Bergen <sakari.bergen@beatwaves.net>
2012-07-17 18:08:07 +0300

    Fix indentation

--- FeatureExtractor/src/inverse_matrix.h
+++ FeatureExtractor/src/inverse_matrix.h
@@ -1,11 +1,10 @@
 #pragma once
 
- #include <boost/numeric/ublas/vector.hpp>
- #include <boost/numeric/ublas/vector_proxy.hpp>
- #include <boost/numeric/ublas/matrix.hpp>
- #include <boost/numeric/ublas/triangular.hpp>
- #include <boost/numeric/ublas/lu.hpp>
- #include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/triangular.hpp>
+#include <boost/numeric/ublas/lu.hpp>
 
 namespace cf {
 namespace FeatureExtractor {
