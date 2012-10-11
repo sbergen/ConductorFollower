e374498418376418cf42e2e8b0c789d6f5171673
Sakari Bergen <sakari.bergen@beatwaves.net>
2012-10-11 14:07:49 +0300

    Make the default catchup fraction smaller

--- ScoreFollower/ScoreFollower/FollowerOptions.h
+++ ScoreFollower/ScoreFollower/FollowerOptions.h
@@ -8,7 +8,7 @@ namespace Options {
 
 typedef StringStatusItem<cf::Status::Setting, cf::Status::File> FileType;
 typedef BangStatusItem<cf::Status::Setting> BangType;
-typedef FloatStatusItem<cf::Status::Setting, cf::Status::Bar, 50, 0, 100> PercentType;
+typedef FloatStatusItem<cf::Status::Setting, cf::Status::Bar, 35, 0, 100> CatchupFractionType;
 typedef FloatStatusItem<cf::Status::Setting, cf::Status::Bar, 2500, 100, 5000> TimeFilterCoefType;
 
 CF_STATUS_GROUP
@@ -17,7 +17,7 @@ FollowerOptions,
 	((ScoreDefinition, "Score definition file", FileType))
 	((Restart, "Restart score", BangType))
 	((Listen, "Listen to score", BangType))
-	((CatchupFraction, "Cathup fraction (%)", PercentType))
+	((CatchupFraction, "Cathup fraction (%)", CatchupFractionType))
 	((TempoFilterTime, "Tempo filter cutoff time (ms)", TimeFilterCoefType))
 )
 
